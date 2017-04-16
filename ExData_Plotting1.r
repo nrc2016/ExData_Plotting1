7ddf84d44d65e81defdcab101895b3cfcd936757
M Z <marvin.zaluski@nrc.ca>
2017-04-16 12:26:19 -0400

    New R scripts for the required plots

---
+++ ExData_Plotting1.r
@@ -0,0 +1,34 @@
+##############################################################################
+#
+# Filename: ExData_Plotting1.r
+#
+# This file defines the functionality for creating the four plots required for
+# the week 1 project in the Exploratory Data Analysis course. The program can
+# executed using the mainline() function.
+#
+# Functions:
+#   - mainline
+#
+# Date: April 16, 2017
+#
+##############################################################################
+
+mainline <- function() {
+  print("Starting mainline ...")
+
+  # create first plot
+  source("plot1.R")
+  
+  # create second plot
+  source("plot2.R")
+  
+  # create third plot
+  source("plot3.R")
+
+  # create fourth plot
+  source("plot4.R")
+  
+  print("... End mainline")
+}
+
+mainline()
---
+++ plot2.R
@@ -0,0 +1,96 @@
+# global variables
+data.dir <- "data"
+dataset.filename <- paste(data.dir, "household_power_consumption.txt", sep="/")
+start.datetime.string = "2007-02-01"
+end.datetime.string = "2007-02-03"
+start.datetime <- as.POSIXct(start.datetime.string, format="%Y-%m-%d")
+end.datetime <- as.POSIXct(end.datetime.string, format="%Y-%m-%d")
+plot.filename <- "plot2.png"
+dataset.cache.name <- "household.power.comsumption"
+required.features <- c("datetime",
+                       "globalactivepower",
+                       "submetering1",
+                       "submetering2",
+                       "submetering3",
+                       "voltage",
+                       "globalreactivepower")
+
+check.required.features <- function (df) {
+  result <- required.features %in% colnames(df)
+  
+  if (length(which(result==FALSE)) > 0) {
+    return(FALSE)
+  }
+  
+  return(TRUE)
+}
+
+load_data <- function (filename) {
+  print("...... Loading data ...")
+  # check if data set exists
+  if (!file.exists(filename)) {
+    return(paste("ERROR: The file ", filename, " does not exist."))
+  }
+  
+  #check if data set was previously cached with required features
+  if (exists(dataset.cache.name)) {
+    df <- get(dataset.cache.name)
+    if (check.required.features(df)) {
+      print("......... Found cached data set ...")
+      print("......... End loading data.")
+      return(df)
+    }
+  }
+  
+  # load data set and change missing values to NA
+  df <- read.csv(filename, sep=";" , na.strings="?")
+  
+  # convert date/time
+  df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+  
+  # filter only dates between 2007-02-01 and 2007-02-02
+  df <- df[df$DateTime > start.datetime & df$DateTime < end.datetime,]
+  
+  # format the feature names
+  colnames(df) <- tolower(colnames(df))
+  colnames(df) <- gsub("_", "", colnames(df), fixed=TRUE)
+  
+  # save to global environment
+  assign(dataset.cache.name, df, envir=.GlobalEnv)
+  
+  print("......... End loading data.")
+  return(df)
+}
+
+create_plot_2 <- function () {
+  print("... Start creating plot 2 ...")
+  
+  # load data
+  df <- load_data(dataset.filename)
+  if (class(df) != "data.frame") {
+    print(df)
+    return(FALSE)
+  }
+  
+  # open graphics device
+  print(paste("...... Opening PNG file", plot.filename, "..."))
+  png(plot.filename, width=480, height=480)
+  print(paste("......... End opening PNG file."))
+  
+  # create plot
+  print("...... Creating Global Active Power plot ...")
+  plot(x=df$datetime, y=df$globalactivepower,
+       type="l",
+       xlab="",
+       ylab="Global Active Power (kilowatts)")
+  print("......... End creating Global Active Power plot.")
+  
+  # close graphics device
+  print("...... Closing graphics device ...")
+  dev.off()
+  print("......... End closing graphics device.")
+  
+  print("...... End create plot 2.")
+}
+
+create_plot_2()
